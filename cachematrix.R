8584aac7cec5d45ff2468f3de1f2ceae7f6450fa
tcw176 <tcw176@users.noreply.github.com>
2015-01-18 23:01:39 -0500

    Assignment 2 Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,19 @@
 ## Function makeCacheMatrix takes argument x, then sets and gets the inverse of the matrix.
 ## If the matrix is null, the result m is stored as NULL.
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
-
 
 ## Function cacheSolve takes the argument x which is the is the function that has been assinged makeCacheMatrix
 ## It checks to see if the inverse of the matrix has already been calculated, and if so gets
