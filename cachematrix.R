05df416fc500e04fafdee025702f816e92ae65d7
tcw176 <tcw176@users.noreply.github.com>
2015-01-18 22:36:13 -0500

    Solution to Programming A

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix takes argument x, then sets and gets the inverse of the matrix.
+## If the matrix is null, the result m is stored as NULL.
 
 ## Write a short comment describing this function
 
@@ -8,9 +8,19 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve takes the argument x which is the is the function that has been assinged makeCacheMatrix
+## It checks to see if the inverse of the matrix has already been calculated, and if so gets
+## and returns the inverse of the matrix.
+## If the there is no value it returns an error.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
-## Test commits
